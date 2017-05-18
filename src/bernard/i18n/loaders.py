0c784fbc38dda883f7393c172f51f5c209bbcb4a
Rémy Sanchez <remy.sanchez@hyperthese.net>
2017-05-18 12:21:37 +0200

    Also load intents

--- src/bernard/i18n/loaders.py
+++ src/bernard/i18n/loaders.py
@@ -3,9 +3,13 @@ import csv
 from typing import Callable, Dict, Text, List
 
 
-class BaseLoader(object):
+TransDict = Dict[Text, Text]
+IntentDict = Dict[Text, List[Text]]
+
+
+class BaseTranslationLoader(object):
     """
-    Base skeleton for a loader.
+    Base skeleton for a translations loader.
 
     Loaders must have an asynchronous `load` function that will be called with
     kwargs only. This function must load the translations and trigger an
@@ -13,9 +17,9 @@ class BaseLoader(object):
     """
 
     def __init__(self):
-        self.listeners = []  # type: List[Callable[[Dict[Text, Text]]], None]
+        self.listeners = []  # type: List[Callable[[TransDict]], None]
 
-    def on_update(self, cb: Callable[[Dict[Text, Text]], None]) -> None:
+    def on_update(self, cb: Callable[[TransDict], None]) -> None:
         """
         Registers an update listener
 
@@ -23,7 +27,7 @@ class BaseLoader(object):
         """
         self.listeners.append(cb)
 
-    def _update(self, data: Dict[Text, Text]):
+    def _update(self, data: TransDict):
         """
         Propagate updates to listeners
 
@@ -42,7 +46,7 @@ class BaseLoader(object):
         raise NotImplementedError
 
 
-class CsvLoader(BaseLoader):
+class CsvTranslationLoader(BaseTranslationLoader):
     """
     Loads data from a CSV file
     """
@@ -57,4 +61,65 @@ class CsvLoader(BaseLoader):
         with open(file_path, newline='', encoding='utf-8') as f:
             reader = csv.reader(f)
             data = {k: v for k, v in reader}
+        self._update(data)
+
+
+class BaseIntentsLoader(object):
+    """
+    Base skeleton for an intents loader.
+
+    Loaders must have an asynchronous `load` function that will be called with
+    kwargs only. This function must load the translations and trigger an
+    update event. It must NOT finish before the update is done.
+    """
+
+    def __init__(self):
+        self.listeners = []  # type: List[Callable[[IntentDict]], None]
+
+    def on_update(self, cb: Callable[[IntentDict], None]) -> None:
+        """
+        Registers an update listener
+
+        :param cb: Callback that will get called on update
+        """
+        self.listeners.append(cb)
+
+    def _update(self, data: IntentDict):
+        """
+        Propagate updates to listeners
+
+        :param data: Data to propagate
+        """
+
+        for l in self.listeners:
+            l(data)
+
+    async def load(self, **kwargs) -> None:
+        """
+        Starts the load cycle. Data must be loaded at least once before this
+        function finishes.
+        """
+
+        raise NotImplementedError
+
+
+class CsvIntentsLoader(BaseIntentsLoader):
+    """
+    Load intents from a CSV
+    """
+
+    async def load(self, file_path: Text):
+        """
+        Load data from a Excel-formatted CSV file.
+
+        :param file_path: path to the file to read 
+        """
+
+        with open(file_path, newline='', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            data = {}
+
+            for k, v in reader:
+                data[k] = data.get(k, []) + [v]
+
         self._update(data)
--- tests/issue_0001/test_i18n.py
+++ tests/issue_0001/test_i18n.py
@@ -2,23 +2,40 @@
 import asyncio
 import os
 import pytest
-from bernard.i18n.loaders import BaseLoader, CsvLoader
+from bernard.i18n.loaders import BaseTranslationLoader, CsvTranslationLoader, \
+    BaseIntentsLoader, CsvIntentsLoader
 from unittest.mock import Mock
 
 
+def run(task):
+    asyncio.get_event_loop().run_until_complete(task)
+
+
 # noinspection PyProtectedMember
-def test_events_spreading():
+def test_translations_events_spreading():
     mock_cb = Mock()
     data = {'updated': 'yes'}
 
-    loader = BaseLoader()
+    loader = BaseTranslationLoader()
     loader.on_update(mock_cb)
     loader._update(data)
 
     mock_cb.assert_called_once_with(data)
 
 
-def test_load_csv():
+# noinspection PyProtectedMember
+def test_intents_events_spreading():
+    mock_cb = Mock()
+    data = {'updated': ['yes']}
+
+    loader = BaseIntentsLoader()
+    loader.on_update(mock_cb)
+    loader._update(data)
+
+    mock_cb.assert_called_once_with(data)
+
+
+def test_load_translations_csv():
     mock_cb = Mock()
     data = {
         'FOO': 'éléphant',
@@ -30,14 +47,39 @@ def test_load_csv():
         'trans.csv',
     )
 
-    loader = CsvLoader()
+    loader = CsvTranslationLoader()
     loader.on_update(mock_cb)
-    asyncio.get_event_loop().run_until_complete(loader.load(file_path))
+    run(loader.load(file_path=file_path))
 
     mock_cb.assert_called_once_with(data)
 
 
-def test_base_loader_is_abstract():
-    loader = BaseLoader()
+def test_base_translations_loader_is_abstract():
+    loader = BaseTranslationLoader()
     with pytest.raises(NotImplementedError):
-        asyncio.get_event_loop().run_until_complete(loader.load())
+        run(loader.load())
+
+
+def test_base_intents_loader_is_abstract():
+    loader = BaseIntentsLoader()
+    with pytest.raises(NotImplementedError):
+        run(loader.load())
+
+
+def test_load_intents_csv():
+    mock_cb = Mock()
+    data = {
+        'FOO': ['bar', 'baz'],
+        'BAR': ['ᕕ( ՞ ᗜ ՞ )ᕗ', '∩༼˵☯‿☯˵༽つ¤=[]:::::>', 'c( ⁰ 〰 ⁰ )੭'],
+    }
+    file_path = os.path.join(
+        os.path.dirname(__file__),
+        'assets',
+        'intents.csv',
+    )
+
+    loader = CsvIntentsLoader()
+    loader.on_update(mock_cb)
+    run(loader.load(file_path=file_path))
+
+    mock_cb.assert_called_once_with(data)
