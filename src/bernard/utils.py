61e93ecdf349105d1aee02bdb1e78e60cb057f9c
Rémy Sanchez <remy.sanchez@hyperthese.net>
2017-05-25 12:49:49 +0200

    Class expression engine

--- src/bernard/utils.py
+++ src/bernard/utils.py
@@ -1,8 +1,9 @@
 # coding: utf-8
+import re
 import importlib
 import asyncio
 from collections import Sequence, Mapping
-from typing import Text, Coroutine, Any, Union, Dict, Iterator
+from typing import Text, Coroutine, Any, Union, Dict, Iterator, List
 
 
 def import_class(name: Text) -> type:
@@ -120,3 +121,41 @@ def make_ro(obj: Any, forgive_type=False):
     else:
         raise ValueError('Trying to make read-only an object of type "{}"'
                          .format(obj.__class__.__name__))
+
+
+class ClassExp(object):
+    """
+    Perform regular expression matching on list of classes.
+    """
+
+    RE_SPACES = re.compile(r'\s+')
+    RE_PYTHON_VAR = re.compile(r'([A-Za-z_][A-Za-z_0-9]*)')
+
+    def __init__(self, expression):
+        self._initial_expression = expression
+        self._compiled_expression = self._compile(expression)
+
+    def _compile(self, expression):
+        """
+        Transform a class exp into an actual regex
+        """
+
+        x = self.RE_SPACES.sub('', expression)
+        x = self.RE_PYTHON_VAR.sub('(:?\\1,)', x)
+        return re.compile(x)
+
+    def _make_string(self, objects: List[Any]) -> Text:
+        """
+        Transforms a list of objects into a matchable string
+        """
+
+        return ''.join(x.__class__.__name__ + ',' for x in objects)
+
+    def match(self, objects: List[Any]) -> bool:
+        """
+        Return True if the list of objects matches the expression.
+        """
+
+        s = self._make_string(objects)
+        m = self._compiled_expression.match(s)
+        return m is not None
---
+++ tests/issue_0001/test_class_exp.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+from bernard.utils import ClassExp
+
+
+# noinspection PyProtectedMember
+def test_compile():
+    ce = ClassExp(r'(RawText|Text)+ QuickRepliesList?')
+    expect = "re.compile('((:?RawText,)|(:?Text,))+(:?QuickRepliesList,)?')"
+    assert repr(ce._compiled_expression) == expect
+
+
+# noinspection PyProtectedMember
+def test_make_string():
+    ce = ClassExp(r'')
+    expect = 'int,bool,int,bool,'
+    assert ce._make_string([42, True, 42, False]) == expect
+
+
+def test_match():
+    ce = ClassExp(r'(int|bool)+ float')
+    assert ce.match([42, 42, 42, True, 42, 42.0])
+    assert not ce.match([42, True, 42, True])
