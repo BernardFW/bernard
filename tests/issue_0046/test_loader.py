54562e876e96470ac30e732a2e2f218efff4e9ee
Rémy Sanchez <remy.sanchez@hyperthese.net>
2018-01-31 19:18:38 +0100

    Load negative intents
    BernardFW/bernard#46

---
+++ tests/issue_0046/test_loader.py
@@ -0,0 +1,14 @@
+from bernard.i18n.loaders import extract_ranges
+
+
+def test_extract_ranges():
+    row = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+    ranges = [
+        0,
+        (1, 2),
+        (8, None),
+    ]
+
+    extracted = extract_ranges(row, ranges)
+
+    assert extracted == ['1', '2', '9', '10']
