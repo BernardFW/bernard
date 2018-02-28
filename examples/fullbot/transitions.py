a78f8a15139b7be00abd58233aedad8f59077f92
Rémy Sanchez <remy.sanchez@hyperthese.net>
2018-02-28 21:06:53 +0100

    Handle receiving images
    BernardFW/bernard#50

--- examples/fullbot/transitions.py
+++ examples/fullbot/transitions.py
@@ -1,8 +1,9 @@
 # coding: utf-8
 
 from bernard.engine import Tr, triggers
+from bernard import layers as lyr
 from bernard.i18n import intents
-from .states import Text, Keyboard, Locale
+from .states import Text, Keyboard, Locale, TelegramPhoto
 
 transitions = [
     Tr(Text, triggers.Text.builder(intents.TEXT)),
@@ -14,4 +15,6 @@ transitions = [
        factory=triggers.Action.builder('notif')),
     Tr(dest=Keyboard, do_not_register=True,
        factory=triggers.Action.builder('alert')),
+    Tr(dest=TelegramPhoto, do_not_register=True,
+       factory=triggers.Layer.builder(lyr.Image)),
 ]
