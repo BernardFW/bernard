0a63693048ccab7d2804cab284c67eab59e3a089
Laurent Tréguier <laurent.treguier@with-madrid.com>
2022-11-25 12:31:05 +0100

    chore: bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="bernard",
-    version="0.2.0",
+    version="0.3.0",
     packages=find_packages("src"),
     package_dir={
         "": "src",
